20e8b19cb18f6783e46350b333ea78dbb9225132
Ilya Zayats <somebody32@gmail.com>
2011-03-31 12:47:35 +0400

    added category filter

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,13 @@ class ArticlesController < ApplicationController
   
   def index
     Scrapper.update_feed
-    @articles = Article.all
+    kind = nil
+    if %w(uni social science culture sport).include? params[:kind]
+      @articles = Article.where(:kind => params[:kind])
+    else
+      @articles = Article.all
+    end
+    
     
     respond_to do |format|
       format.rss
