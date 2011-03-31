ecefc8144881cd8c5abc7cdf7535da948c33c470
Ilya Zayats <somebody32@gmail.com>
2011-03-31 12:25:50 +0400

    trying to invoke rake task from the model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
   
   def index
-    call_rake('scrap')
+    #call_rake('scrap')
+    Article.update_feed
     @articles = Article.all
     
     respond_to do |format|
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
+require 'rake'
 class Article < ActiveRecord::Base
   validates_presence_of :link_id, :title, :kind
+  
+  def self.update_feed
+    load File.join(Rails.root, 'lib', 'tasks', 'scrapper.rake')
+    Rake::Task[:scrap].invoke
+  end
 end
