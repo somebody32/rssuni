84d9ae40b05068e73377b48ac3e204121a92a25a
Ilya Zayats <somebody32@gmail.com>
2011-04-14 10:47:14 +0400

    updated scraper for the new uni design

--- lib/scrap.rb
+++ lib/scrap.rb
@@ -5,14 +5,20 @@ require 'open-uri'
 url = "http://iuni.ru/articles/uni"  
 doc = Nokogiri::HTML(open(url)) 
  
-doc.css(".news, .newsBig").each do |item|
-  puts item[:class]
-  link = item.at_css("a.title")
+doc.css(".info").each do |item|
+  heading  = item.at_css("h3") || item.at_css("h1")
+  next unless heading
+  
+  link = heading.at_css("a")
+  id = link[:href][/[0-9]+/].to_i
   puts link.text
-  #  id = link[:href][/[0-9]+/]
-  # 
-  #  unless Article.find_by_link_id(id)
-  #    desc = item.at_css(".subTitle") || item.at_css(".subtitle")
-  #    Article.create(:link_id => id, :title => link.text, :kind => kind, :description => desc.try(:text))   
-  #  end
+  
+  desc = item.at_css("h4") || item.at_css("h2")
+  
+  puts desc.text
+  
+  # unless articles_ids.include?(id)
+  #     desc = item.at_css("h2") || item.at_css("h4")
+  #     Article.create(:link_id => id, :title => link.text, :kind => kind, :description => desc.try(:text))   
+  #   end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,12 +11,15 @@ module Scraper
         url = "http://iuni.ru/articles/#{kind}"  
         doc = Nokogiri::HTML(open(url)) 
 
-        doc.css(".news, .newsBig").each do |item|
-          link = item.at_css("a.title")
+        doc.css(".info").each do |item|
+          heading  = item.at_css("h3") || item.at_css("h1")
+          next unless heading
+          
+          link = heading.at_css("a")
           id = link[:href][/[0-9]+/].to_i
 
           unless articles_ids.include?(id)
-            desc = item.at_css(".subTitle") || item.at_css(".subtitle")
+            desc = item.at_css("h4") || item.at_css("h2")
             Article.create(:link_id => id, :title => link.text, :kind => kind, :description => desc.try(:text))   
           end
         end
